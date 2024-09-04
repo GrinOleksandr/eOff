95c361c59eec52c4dba1d57c8bbff95ece0192f3
Oleksandr H <1nutak1@gmail.com>
2024-09-04 19:58:53 +0300

    added support for minutes

--- src/common/utils.ts
+++ src/common/utils.ts
@@ -2,6 +2,12 @@ import { TelegramClient } from 'telegram';
 import config from '../config';
 import moment from 'moment-timezone';
 
+export interface DateObj {
+  year: number;
+  name: string;
+  index: number;
+}
+
 export const MONTH_NAMES: string[] = [
   'січня',
   'лютого',
@@ -17,23 +23,23 @@ export const MONTH_NAMES: string[] = [
   'грудня',
 ];
 
-const getCurrentMonth = (): { year: number; name: string; index: number } => {
-  const currentDate = new Date();
-  const currentMonth = currentDate.getMonth(); // Returns a number (0-11)
+const getCurrentMonth = (): DateObj => {
+  const currentDate = getNewKyivDate();
+  const currentMonth = currentDate.month(); // Returns a number (0-11)
 
-  return { index: currentMonth + 1, name: MONTH_NAMES[currentMonth], year: currentDate.getFullYear() };
+  return { index: currentMonth + 1, name: MONTH_NAMES[currentMonth], year: currentDate.year() };
 };
 
-const getNextMonth = (): { year: number; name: string; index: number } => {
-  const currentDate = new Date();
-  let nextMonthIndex = currentDate.getMonth() + 1; // Increment to get next month
-  const currentYear = currentDate.getFullYear();
+const getNextMonth = (): DateObj => {
+  const currentDate = getNewKyivDate();
+  let nextMonthIndex = currentDate.month() + 1; // Increment to get next month
+  const currentYear = currentDate.year();
 
   let year = currentYear;
   // Handle wrap around for December
   if (nextMonthIndex === 12) {
     nextMonthIndex = 0; // January (0 index)
-    if (currentDate.getDate() === 31) {
+    if (currentDate.date() === 31) {
       year = currentYear + 1;
     }
   }
@@ -51,16 +57,16 @@ function formatDateFromObject(obj: { day: any; index: any; name?: string; year?:
   return `${year}-${month}-${day}`;
 }
 
-function getTodayDate(): string {
-  const today = new Date();
-  const year = today.getFullYear(); // Get the year (YYYY)
-  const month = (today.getMonth() + 1).toString().padStart(2, '0'); // Get the month (MM) and pad with leading zero if
-  // necessary
-  const day = today.getDate().toString().padStart(2, '0'); // Get the day (DD) and pad with leading zero if necessary
-
-  // Construct date string in YYYY-MM-DD format
-  return `${year}-${month}-${day}`;
-}
+// function getTodayDate(): string {
+//   const today = getNewKyivDate();
+//   const year = today.year(); // Get the year (YYYY)
+//   const month = (today.month() + 1).toString().padStart(2, '0'); // Get the month (MM) and pad with leading zero if
+//   // necessary
+//   const day = today.date().toString().padStart(2, '0'); // Get the day (DD) and pad with leading zero if necessary
+//
+//   // Construct date string in YYYY-MM-DD format
+//   return `${year}-${month}-${day}`;
+// }
 
 let tgClient: TelegramClient;
 
@@ -96,7 +102,7 @@ export {
   getCurrentMonth,
   getNextMonth,
   formatDateFromObject,
-  getTodayDate,
+  // getTodayDate,
   getTelegramClient,
   getFormattedDate,
   getNewKyivDate,
