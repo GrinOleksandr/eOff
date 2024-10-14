782dae46d968ea1af95250a31a661950a7c2d7b9
Oleksandr Hrin <iamsashagrin@gmail.com>
2024-10-14 17:08:39 +0300

    final refactoring

--- src/config.ts
+++ src/config.ts
@@ -3,13 +3,12 @@ import { StringSession } from 'telegram/sessions/index.js';
 import { IConfig } from './common/types-and-interfaces';
 
 const config: IConfig = {
-  providerName: 'CHERKOE',
   telegram: {
     apiId: parseInt(process.env.TELEGRAM_APP_ID || ''),
     apiHash: process.env.TELEGRAM_API_HASH || '',
     stringSession: new StringSession(process.env.TELEGRAM_SESSION_STRING || ''), // Empty string for new session
-    channelUsername: process.env.TELEGRAM_TARGET_CHANNEL_NAME || 'pat_cherkasyoblenergo', //channel to listen
-    MESSAGES_LIMIT: parseInt(process.env.MESSAGES_LIMIT || '') || 20,
+    cherkoeChannel: process.env.TELEGRAM_CHERKOE_CHANNEL_NAME || '', //channel to listen
+    MESSAGES_LIMIT: parseInt(process.env.MESSAGES_LIMIT || '') || 30,
   },
 };
 
