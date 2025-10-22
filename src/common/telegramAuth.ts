46652a4ab9d595ff3bfd5d2de306c3b69d2b2ac2
Oleksandr H <1nutak1@gmail.com>
2025-10-22 14:35:38 +0300

    [BUGFIX] refactor

--- src/common/telegramAuth.ts
+++ src/common/telegramAuth.ts
@@ -5,8 +5,8 @@ import { TelegramClient } from 'telegram';
 import { StringSession } from 'telegram/sessions/index.js';
 import config from '../config';
 
-const apiId = 23459920; // Replace with your actual api_id
-const apiHash = 'b7095f3da2c7edadb11fcd95240c4067'; // Replace with your actual api_hash
+const apiId = 11111; // Replace with your actual api_id
+const apiHash = ''; // Replace with your actual api_hash
 const stringSession = new StringSession(''); // Empty string to start with a new session, don't edit it here
 
 import * as readline from 'readline';
