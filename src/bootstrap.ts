a9349133404c0296dda986b14e1a3a55f12c172d
Oleksandr H <1nutak1@gmail.com>
2026-02-05 20:55:55 +0200

    [FEAT] fix bootstrap

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -1,1 +1,8 @@
-import './app';
+import { App } from './app';
+
+// Create and start the app
+const appInstance = new App();
+appInstance.startServer();
+
+// Export for Vercel (if this is your entry point for Vercel)
+export default appInstance.app;
